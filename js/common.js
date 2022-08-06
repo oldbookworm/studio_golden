ea8c4a971b32b75d960577998c2cdb873aaf151b
Ann <shokh.anna@mail.ru>
2022-08-06 21:34:39 +0300

    Clients. Add slider

--- js/common.js
+++ js/common.js
@@ -45,3 +45,37 @@ jQuery(document).ready(function(){
         })
     });
   });
+
+// slick слайдер
+$(document).ready(function(){
+  $('.clients_list').slick({
+    infinite: true,
+    slidesToShow: 4,
+    arrows: false,
+  	responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    ]
+  });
+});
+
